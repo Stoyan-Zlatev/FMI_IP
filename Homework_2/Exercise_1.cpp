d7a285a3d8f29ec81b5404149801020d764b3bac
Стоян Златев <47269030+Tony5768@users.noreply.github.com>
2022-03-20 16:51:46 +0200

    Update Exercise_1.cpp

--- Homework_2/Exercise_1.cpp
+++ Homework_2/Exercise_1.cpp
@@ -1,1 +1,56 @@
+#include <iostream>
 
+using namespace std;
+
+unsigned int GetBinaryBase(unsigned int n)
+{
+	unsigned int binaryBase = 1;
+	while (n > 1)
+	{
+		binaryBase *= 10;
+		n /= 2;
+	}
+	return binaryBase;
+}
+
+unsigned int ConvertToBinary(unsigned int n)
+{
+	unsigned int multiplicator = 1;
+	unsigned int binaryNum = GetBinaryBase(n);
+	
+	while (n > 1)
+	{
+		binaryNum += (multiplicator * (n % 2));
+		n /= 2;
+		multiplicator *= 10;
+	}
+
+	return binaryNum;
+}
+
+bool hasAlternatingBits(unsigned n)
+{
+	while (n > 10)
+	{
+		unsigned int bit1 = n % 10;
+		unsigned int bit2 = (n /= 10) % 10;
+		if (bit1 == bit2)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	int n;
+	if (n < 0)
+	{
+		cout << "-1";
+		return 0;
+	}
+
+	cin >> n;
+	cout << boolalpha << hasAlternatingBits(ConvertToBinary(n));
+}
